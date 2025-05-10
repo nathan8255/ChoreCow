24f30139a5a71164b086f1eee870ba8c8c380f3b
NathanJoannette <njoannette@conestogac.on.ca>
2025-05-10 17:27:46 -0400

    Added outline for complete command and updated name of bot from chore lizard -> chore cow

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { config } from 'dotenv';
 config();
-import { Client, Routes } from 'discord.js';
+import { Client, Routes, SlashCommandBuilder } from 'discord.js';
 import { REST } from '@discordjs/rest';
 
 const TOKEN = process.env.BOT_TOKEN;
@@ -10,12 +10,18 @@ const GUILD_ID = '642854722402779146';
 const client = new Client({ intents: [] });
 const rest = new REST({ version: '10' }).setToken(TOKEN);
 
-client.on('ready', () => console.log('The lizard has awoken'));
+client.on('ready', () => console.log('The cow has awoken'));
+
+const commands = [
+    new SlashCommandBuilder().setName('complete')
+    .setDescription('Stop the cow from nudging you by marking your weekly chores as complete')
+    .toJSON(),
+];
 
 async function main() {
   try {
     await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
-      body: [],
+      body: commands,
     });
     client.login(TOKEN);
   } catch (err) {
